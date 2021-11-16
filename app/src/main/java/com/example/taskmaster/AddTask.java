99668f8f8cb51511b8729668ba6034b6174abeea
OdehYazan <eng.yazanodeh@gmail.com>
2021-11-16 20:48:28 +0200

    lab33

--- app/src/main/java/com/example/taskmaster/AddTask.java
+++ app/src/main/java/com/example/taskmaster/AddTask.java
@@ -1,11 +1,14 @@
 package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.amplifyframework.api.graphql.model.ModelMutation;
@@ -13,6 +16,8 @@ import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.generated.model.Task;
 
 public class AddTask extends AppCompatActivity {
+
+    private final String TAG="AddTask";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,21 +43,43 @@ public class AddTask extends AppCompatActivity {
                 EditText taskStateInput= findViewById(R.id.inputTaskState);
                 String taskState = taskStateInput.getText().toString();
 
+                RadioButton b1 = findViewById(R.id.team1);
+                RadioButton b2 = findViewById(R.id.team2);
+                RadioButton b3 = findViewById(R.id.team3);
+
+                String id = null;
+                if (b1.isChecked()) {
+                    id = "1";
+                } else if (b2.isChecked()) {
+                    id = "2";
+                } else if (b3.isChecked()) {
+                    id = "3";
+                }
+//                dataStore(taskTitle, taskBody, taskState, id);
+
+                Intent intent = new Intent(AddTask.this, MainActivity.class);
+                startActivity(intent);
+
+
+                /////////////////////////first AWS try///////////////////////////////////
                      Task todo = Task.builder()
-                        .title(taskTitle)
-                        .body(taskBody)
-                        .state(taskState)
-                        .build();
+                             .teamId(id)
+                             .title(taskTitle)
+                             .body(taskBody)
+                             .state(taskState)
+                             .build();
 
                   Amplify.API.mutate(
                         ModelMutation.create(todo),
-                        response -> Log.i("AddTask", "Added Todo with id: " + response.getData().getId()),
-                        error -> Log.e("AddTask", "Create failed", error)
+                        response -> Log.i(TAG, "Added Todo with id: " + response.getData().getId()),
+                        error -> Log.e(TAG, "Create failed", error)
                 );
-//
+//         /////////////////////////first AWS try///////////////////////////////////
+
+
 //                Task task= new Task(taskTitle,taskBody,taskState);
 //
-//           Long addTaskID = AppDatabase.getInstance(getApplicationContext()).taskDao().insertTask(task);
+//                 Long addTaskID = AppDatabase.getInstance(getApplicationContext()).taskDao().insertTask(task);
 //
 //                System.out.println(
 //                        "****************************************************************"
@@ -62,4 +89,15 @@ public class AddTask extends AppCompatActivity {
         });
 
     }
+//    private void dataStore(String title, String body, String state,String id) {
+//        Task task = Task.builder().teamId(id).title(title).body(body).state(state).build();
+//        Amplify.API.mutate(
+//                ModelMutation.create(task),
+//                response -> Log.i(TAG, "Added Todo with id: " + response.getData().getId()),
+//                error -> Log.e(TAG, "Create failed", error)
+//        );
+//
+//
+//    }
+
 }
